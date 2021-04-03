188c0c87c11c0060ca6782c863092991f65d97d2
DaviNakamuraCardoso <davi.nakamura01@gmail.com>
2021-04-02 23:03:52 -0300

    Using signals

---
+++ chapter_10/diediedie/hello.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+
+
+void diediedie(int sig)
+{
+    puts("\nGoodbye cruel world!");
+    exit(1);
+}
+
+int catch_signal(int sig, void (*handler) (int))
+{
+    struct sigaction action;
+    action.sa_handler = handler;
+    sigemptyset(&action.sa_mask);
+    action.sa_flags = 0;
+
+    return sigaction(sig, &action, NULL);
+}
+
+int main(void)
+{
+    if (catch_signal(SIGINT, diediedie) == -1)
+    {
+        fprintf(stderr, "Can't map the handler.\n");
+        exit(2);
+    }
+
+    char name[30];
+    printf("Enter your name: ");
+    fgets(name, 30, stdin);
+    printf("Hello, %s\n", name);
+
+    return 0;
+
+}

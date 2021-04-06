46ac463f965216214823edc64f78ac1dea6e8734
DaviNakamuraCardoso <davi.nakamura01@gmail.com>
2021-04-06 19:00:23 -0300

    Allowing the spaceship to rotate and go in different directions

--- clab_03/source/spaceship.c
+++ clab_03/source/spaceship.c
@@ -9,6 +9,7 @@
 
 #define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
 #define MAX(X, Y) (((X) < (Y)) ? (Y) : (X))
+#define r 30
 
 
 float pi(float deg)
@@ -48,20 +49,18 @@ void draw_spaceship(SPACESHIP* s)
 
     float thickness = 3.0;
 
-    float xo = cos(pi(s->heading + 0));
-    float yo = sin(pi(s->heading + 0));
+    float xo = cos(pi(s->heading + 70));
+    float yo = sin(pi(s->heading + 70));
 
     printf("Xo is %f and Yo is %f\n", xo, yo);
 
-    float xo2 = cos(pi(s->heading + 50));
-    float yo2 = sin(pi(s->heading + 50));
-
-    int r = 12;
+    float xo2 = cos(pi(s->heading + 110));
+    float yo2 = sin(pi(s->heading + 110));
 
 
-    al_draw_line(x1 - (xo * r), y1 + (yo * r), x1 + (xo * r), y1 - (yo * r), s->color, thickness);
-    al_draw_line(x1 + (xo * r), y1 - (yo * r), x1 + (xo * r), y1 + (yo * r), s->color, thickness);
-    // al_draw_line(x1+-6, y1+4, x1-1, y1+4, s->color, thickness);
+    al_draw_line(x1 + (xo2 * r), y1 - (yo2 * r), x1, y1, s->color, thickness);
+    al_draw_line(x1, y1, x1 + (xo * r), y1 - (yo * r), s->color, thickness);
+    al_draw_line(x1 + (xo2* r/ 1.5), y1 - (yo2 * r / 1.5), x1 + (xo * r / 1.5), y1 - (yo * r / 1.5), s->color, thickness);
     // al_draw_line(x1+6, y1+4, x1+1, y1+4, s->color, thickness);
     return;
 }
@@ -72,50 +71,50 @@ void update_spaceship(SPACESHIP* s, ALLEGRO_EVENT event, long double dt)
     float incline_x = cos(pi(s->heading+90));
     float incline_y = sin(pi(s->heading+90));
 
-    s->sx += (s->speed* dt * incline_x);
-    s->sy -= (s->speed* dt * incline_y);
+    s->sx -= (s->speed* dt * incline_x);
+    s->sy += (s->speed* dt * incline_y);
 
     switch (event.keyboard.keycode)
     {
         case ALLEGRO_KEY_UP:
         case ALLEGRO_KEY_W:
         {
-            s->speed = MIN(200, s->speed + 5);
+            s->speed = MIN(400, s->speed + 50 * dt);
             break;
         }
         case ALLEGRO_KEY_DOWN:
         case ALLEGRO_KEY_S:
         {
-            s->speed = MAX(5, s->speed - 5);
+            s->speed = MAX(10, s->speed - 50 * dt);
             break;
         }
         case ALLEGRO_KEY_LEFT:
         case ALLEGRO_KEY_A:
         {
-            s->heading += 10 * (dt) * 5;
+            s->heading += 10 * (dt) * 15;
             break;
         }
         case ALLEGRO_KEY_RIGHT:
         case ALLEGRO_KEY_D:
         {
-            s->heading -= 10 * (dt) * 5;
+            s->heading -= 10 * (dt) * 15;
             break;
         }
     }
 
-    if (s->sy >= WINDOW_HEIGHT)
+    if (s->sy >= WINDOW_HEIGHT + r)
     {
         s->sy = 0;
     }
-    else if (s->sy <= 0)
+    else if (s->sy <= 0 - r)
     {
         s->sy = WINDOW_HEIGHT;
     }
-    if (s->sx <= 0)
+    if (s->sx <= 0 - r)
     {
         s->sx = WINDOW_WIDTH;
     }
-    else if (s->sx >= WINDOW_WIDTH)
+    else if (s->sx >= WINDOW_WIDTH + r)
     {
         s->sx = 0;
     }

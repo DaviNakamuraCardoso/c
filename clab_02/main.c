d21de6725f8f02928519d329ccc263b4420730f2
DaviNakamuraCardoso <davi.nakamura01@gmail.com>
2021-10-02 21:01:50 -0300

    Changes

--- clab_02/main.c
+++ clab_02/main.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char** argv)
 {
-    char* article = "https://en.wikipedia.org/wiki/C_(programming_language)";
+    char* article = "https://en.wikipedia.org/wiki/Adolf_Hitler";
     if (argc >= 2) article = argv[1];
 
     graph(article, 20);

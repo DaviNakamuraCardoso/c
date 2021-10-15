cbf460f5c288b2742f35802b4140177a65d8ee04
DaviNakamuraCardoso <davi.nakamura01@gmail.com>
2021-10-14 23:13:50 -0300

    Basic operations

--- clab_04/src/calculator.c
+++ clab_04/src/calculator.c
@@ -1,8 +1,37 @@
 #include <stdio.h>
 #include <calculator.h>
 
-void calculate(token_t **tokens, stack_t *s, unsigned int length)
+
+ld_t add(a, b)
+    ld_t a, b;
 {
+    return a + b;
+}
+
+ld_t sub(a, b)
+    ld_t a, b;
+{
+    return a - b;
+}
+
+ld_t div(a, b)
+    ld_t a, b;
+{
+    return a / b;
+}
+
+
+ld_t mult(a, b)
+    ld_t a, b;
+{
+    return a * b;
+}
+
+void calculate (token_t **tokens, stack_t *s, unsigned int length)
+{
+    ld_t (*calculations[20]) (ld_t, ld_t);
+    unsigned sp = 0;
+
     for (unsigned int i = 0; i < length; i++)
     {
         if (tokens[i]->t == NUMBER) {
@@ -10,10 +39,14 @@ void calculate(token_t **tokens, stack_t *s, unsigned int length)
         }
         else 
         {
-            ld_t a = pop(s);
-            ld_t b = pop(s);
-            push(s, a+b);
+            calculations[sp++] = add;
         }
+    }
+
+    while (s->sp > 1)
+    {
+        push(s, add(pop(s), pop(s)));
+
     }
 
     return;
